fdce61031940db69bdfa4e0ffaede3d605ed9852
Dev-Ramos <lucaxiDev@gmail.com>
2025-02-19 17:55:09 -0300

    chore(plugins): add eslint sort plugin and prettier tailwindcss plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,16 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import React from 'react'
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 function ProductPage() {
   return (
-    <div>
+    <div className="background-gray-500 rounded-xl border p-4">
       <h1>Product Page</h1>
       <Button>Click me</Button>
       <Input placeholder="Test the shadcn input" />
     </div>
-  )
+  );
 }
- 
-export default ProductPage
+
+export default ProductPage;
