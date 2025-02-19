782a5c5fd4f3da7011329e7348ee09e4ed12851e
Dev-Ramos <lucaxiDev@gmail.com>
2025-02-19 09:45:07 -0300

    feat(styles): add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import React from 'react'
+
+function ProductPage() {
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <Button>Click me</Button>
+      <Input placeholder="Test the shadcn input" />
+    </div>
+  )
+}
+ 
+export default ProductPage
