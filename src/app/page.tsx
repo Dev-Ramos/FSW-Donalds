470d62748ab58d7ada8d8483d26b607a3c6112a6
Dev-Ramos <lucaxiDev@gmail.com>
2025-03-05 15:28:41 -0300

    fix: ensure the restaurant pages be the initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-const InitialPage = () => {
-  redirect("http://localhost:3000/fsw-donalds")
+const InitialPage = async() => {
+  const origin = (await headers()).get('origin') as string
+  redirect(`${origin}/fsw-donalds`)
 }
 
 export default InitialPage
