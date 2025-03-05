c7fdc72f61a96b7088f13977c0be007f7e94ed4d
Dev-Ramos <lucaxiDev@gmail.com>
2025-03-05 15:39:46 -0300

    fix: change the base url depending of environment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-const InitialPage = async() => {
-  const origin = (await headers()).get('origin') as string
-  redirect(`${origin}/fsw-donalds`)
+const InitialPage = async () => {
+  
+  redirect(process.env.NODE_ENV === 'production' ? '/fws-donalds' : 'http://localhost:3000/fsw-donalds')
 }
 
 export default InitialPage
