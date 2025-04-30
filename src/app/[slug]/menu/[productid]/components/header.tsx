769983f781da9c90456d25397bc5f18a3fac32d2
Dev-Ramos <lucaxiDev@gmail.com>
2025-04-30 01:23:12 -0300

    animate: add animation at products details page

--- src/app/[slug]/menu/[productid]/components/header.tsx
+++ src/app/[slug]/menu/[productid]/components/header.tsx
@@ -1,20 +1,25 @@
-'use client'
-import { Product } from "@prisma/client"
-import { ChevronLeftIcon, ShoppingBagIcon } from "lucide-react"
-import Image from "next/image"
-import { useRouter } from "next/navigation"
+"use client";
+import { Product } from "@prisma/client";
+import { motion } from "framer-motion";
+import { ChevronLeftIcon, ShoppingBagIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 interface ProductHeaderProps {
-  product: Pick<Product, "name" | "imageUrl"> 
+  product: Pick<Product, "name" | "imageUrl">;
 }
 
 const ProductHeader = ({ product }: ProductHeaderProps) => {
-  const router = useRouter()
-  const handleBackClick = () => router.back()
+  const router = useRouter();
+  const handleBackClick = () => router.back();
   return (
-    <div className="relative min-h-[300px] w-full">
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="relative min-h-[300px] w-full">
       <Image
         src={product.imageUrl}
         alt={product.name}
@@ -33,12 +38,12 @@ const ProductHeader = ({ product }: ProductHeaderProps) => {
         variant="secondary"
         size="icon"
         className="absolute right-4 top-4 z-50 rounded-full"
-        onClick={()=>router.push('/fws-donalds/orders')}
+        onClick={() => router.push("/fws-donalds/orders")}
       >
         <ShoppingBagIcon />
       </Button>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
-export default ProductHeader
+export default ProductHeader;
--- src/app/[slug]/menu/[productid]/components/product-details.tsx
+++ src/app/[slug]/menu/[productid]/components/product-details.tsx
@@ -1,95 +1,163 @@
-'use client'
-import { Prisma } from "@prisma/client"
-import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react"
-import Image from "next/image"
-import { useContext, useState } from "react"
+"use client";
+import { Prisma } from "@prisma/client";
+import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
+import Image from "next/image";
+import { useContext, useState } from "react";
 
-import { Button } from "@/components/ui/button"
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
-import { priceFormatter } from "@/helpers/currency"
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { priceFormatter } from "@/helpers/currency";
 
-import CartSheet from "../../components/cart-sheet"
-import { CartContext } from "../../context/cart"
+import CartSheet from "../../components/cart-sheet";
+import { CartContext } from "../../context/cart";
 
 interface ProductDetailsProps {
-  product: Prisma.ProductGetPayload<{ include: { restaurant: { select: { name: true, avatarImageUrl: true } } } }>
+  product: Prisma.ProductGetPayload<{
+    include: { restaurant: { select: { name: true; avatarImageUrl: true } } };
+  }>;
 }
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
-  const { toggleCart, addProduct } = useContext(CartContext)
-  const [quantity, setQuantity] = useState<number>(1)
+  const { toggleCart, addProduct } = useContext(CartContext);
+  const [quantity, setQuantity] = useState<number>(1);
   const handleQuantityChange = (option: string) => {
-    if(option === 'decrease'){
-      setQuantity(prev => prev - 1)
-    }else if(option === 'increase'){
-      setQuantity(prev => prev + 1)
+    if (option === "decrease") {
+      setQuantity((prev) => prev - 1);
+    } else if (option === "increase") {
+      setQuantity((prev) => prev + 1);
     }
-  }
-  const handleAddToCart = ()=>{
+  };
+  const handleAddToCart = () => {
     addProduct({
       ...product,
       quantity,
-    })
-    toggleCart()
-  }
-  
+    });
+    toggleCart();
+  };
+
   return (
     <>
       <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col overflow-hidden rounded-t-3xl p-5">
         <div className="flex-auto overflow-hidden">
           {/* Logo and restaurant name */}
-          <div className="flex gap-1.5">
-            <Image src={product.restaurant.avatarImageUrl} alt={product.restaurant.name} width={16} height={16} className="rounded-full"/>
-            <p className="text-xs text-muted-foreground">{product.restaurant.name}</p>
-          </div>
-          {/* Product name */}
-          <h2 className="text-xl font-semibold mt-1">{product.name}</h2>
-          {/* Price and quantity of products */}
-          <div className="flex items-center justify-between mt-3">
-            <h3 className="text-xl font-semibold">
-              {priceFormatter(product.price)}
-            </h3>
-            <div className="flex items-center text-center gap-3">
-              <Button variant='outline' className="rounded-xl w-8 h-8" onClick={()=>handleQuantityChange('decrease')} 
-                disabled={quantity === 1}>
-                <ChevronLeftIcon/>
-              </Button>
-              <p className="w-4">{quantity}</p>
-              <Button variant='destructive' className="rounded-xl w-8 h-8" onClick={()=>handleQuantityChange('increase')}>
-                <ChevronRightIcon/>
-              </Button>
-            </div>
-          </div>
-          <ScrollArea className="h-[65%] mb-5">
-            {/* About product */}
-            <div className="mt-6 space-y-3">
-              <h4 className="font-semibold">Sobre</h4>
-              <p className="text-muted-foreground text-sm">{product.description}</p>
-            </div>
-            {/* Ingerdients */}
-            <div className="mt-6 space-y-3">
-              <div className="flex items-center gap-1 5">
-                <ChefHatIcon size={18}/>
-                <h4 className="font-semibold">Ingredientes</h4>
+          <motion.div
+            initial={{ opacity: 0, x: 20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ type: "spring", bounce: 0.3, duration: 0.8 }}
+            className="flex gap-1.5">
+            <Image
+              src={product.restaurant.avatarImageUrl}
+              alt={product.restaurant.name}
+              width={16}
+              height={16}
+              className="rounded-full"
+            />
+            <p className="text-xs text-muted-foreground">
+              {product.restaurant.name}
+            </p>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+          >
+            {/* Product name */}
+            <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
+            {/* Price and quantity of products */}
+            <div className="mt-3 flex items-center justify-between">
+              <h3 className="text-xl font-semibold">
+                {priceFormatter(product.price)}
+              </h3>
+              <div className="flex items-center gap-3 text-center">
+                <motion.div
+                  whileHover={{ scale: 1.02 }}
+                  whileTap={{ scale: 0.95 }}
+                >
+                  <Button
+                  variant="outline"
+                  className="h-8 w-8 rounded-xl"
+                  onClick={() => handleQuantityChange("decrease")}
+                  disabled={quantity === 1}
+                  >
+                    <ChevronLeftIcon />
+                  </Button>
+                </motion.div>
+                <AnimatePresence mode="wait">
+                  <motion.p
+                    key={quantity}
+                    initial={{ scale: 0}}
+                    animate={{ scale: 1 }}
+                    exit={{ scale: 0 }}
+                    transition={{ duration: 0.1 }}
+                    className="w-4">
+                    {quantity}
+                  </motion.p>
+                </AnimatePresence>
+                <motion.div
+                  whileHover={{ scale: 1.02 }}
+                  whileTap={{ scale: 0.95 }}
+                >
+                  <Button
+                  variant="destructive"
+                  className="h-8 w-8 rounded-xl"
+                  onClick={() => handleQuantityChange("increase")}
+                  >
+                    <ChevronRightIcon />
+                  </Button>
+                </motion.div>
               </div>
-              <ul className="list-disc px-5">
-                {product.ingredients.map((ingredient, index) => (
-                  <li key={index} className="text-muted-foreground text-sm">{ingredient}</li>
-                ))}
-              </ul>
             </div>
-            <ScrollBar orientation='vertical'/>
+          </motion.div>
+          <ScrollArea className="h-[64.5%] mt-2">
+            <motion.div
+              animate={{ opacity: [0, 1], y: [20, 0] }}
+              transition={{ duration: 0.5, delay: 0.3 }}
+            >
+              {/* About product */}
+              <div className="mt-4 space-y-3">
+                <h4 className="font-semibold">Sobre</h4>
+                <p className="text-sm text-muted-foreground">
+                  {product.description}
+                </p>
+              </div>
+              {/* Ingerdients */}
+              <div className="mt-6 space-y-3">
+                <div className="5 flex items-center gap-1">
+                  <ChefHatIcon size={18} />
+                  <h4 className="font-semibold">Ingredientes</h4>
+                </div>
+                <ul className="list-disc px-5">
+                  {product.ingredients.map((ingredient, index) => (
+                    <li key={index} className="text-sm text-muted-foreground">
+                      {ingredient}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <ScrollBar orientation="vertical" />
+            </motion.div>
           </ScrollArea>
-          <div className="absolute bottom-0 left-0 right-0 h-16 bg-white p-5 flex items-center justify-center">
-            <Button variant='default' className="w-full rounded-full " onClick={handleAddToCart}>
-              Adicionar à sacola
-            </Button>
+          <div className="absolute bottom-0 left-0 right-0 flex h-16 items-center justify-center bg-white p-5">
+            <motion.div
+              whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.98 }}
+              className="w-full"
+            >
+              <Button
+                variant="default"
+                className="w-full rounded-full"
+                onClick={handleAddToCart}
+              >
+                Adicionar à sacola
+              </Button>
+            </motion.div>
           </div>
         </div>
       </div>
-      <CartSheet/>
+      <CartSheet />
     </>
-  )
-}
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
